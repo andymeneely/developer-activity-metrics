6d6de8d92aa8008c947fb9846332dbf6ca74278e
apmeneel <andy.meneely@gmail.com>
2010-08-09 15:00:25 -0400

    made the centrality analysis more usable from external classes, switch algorithms.

--- src/edu/ncsu/csc/realsearch/apmeneel/devactivity/analysis/DeveloperNetworkAnalysis.java
+++ src/edu/ncsu/csc/realsearch/apmeneel/devactivity/analysis/DeveloperNetworkAnalysis.java
@@ -15,17 +15,17 @@ import edu.ncsu.csc.realsearch.apmeneel.devactivity.devnetwork.Developer;
 import edu.ncsu.csc.realsearch.apmeneel.devactivity.devnetwork.DeveloperNetwork;
 import edu.ncsu.csc.realsearch.apmeneel.devactivity.devnetwork.FileSet;
 import edu.ncsu.csc.realsearch.apmeneel.devactivity.devnetwork.factory.DBDevAdjacencyFactory;
-import edu.uci.ics.jung.algorithms.importance.BetweennessCentrality;
+import edu.uci.ics.jung.algorithms.scoring.BetweennessCentrality;
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
 import edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics;
 import edu.uci.ics.jung.algorithms.shortestpath.UnweightedShortestPath;
 import edu.uci.ics.jung.graph.Graph;
 
 public class DeveloperNetworkAnalysis {
-	private static org.apache.log4j.Logger log = org.apache.log4j.Logger
-			.getLogger(DeveloperNetworkAnalysis.class);
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(DeveloperNetworkAnalysis.class);
 	private final DBUtil dbUtil;
 	private DeveloperNetwork dn;
+	private BetweennessCentrality<Developer, FileSet> bc;
 
 	public DeveloperNetworkAnalysis(DBUtil dbUtil) {
 		this.dbUtil = dbUtil;
@@ -36,19 +36,16 @@ public class DeveloperNetworkAnalysis {
 		dn = new DBDevAdjacencyFactory(dbUtil).build();
 		log.debug("Running centrality calculations...");
 		loadBetweenness(dn);
-//		log.debug("Running all pairs distance...");
-//		loadDistances(dn, new ComplementFileSetSizeDistance(1787));
+		log.debug("Running all pairs distance...");
+		loadDistances(dn, new ComplementFileSetSizeDistance(1039));
 		// loadDistances(dn, new InverseFileSetSizeDistance());
 		log.debug("Calculating network results...");
 		runNetworkAnalysis(dn);
 	}
 
 	private void loadBetweenness(DeveloperNetwork dn) throws SQLException {
-		BetweennessCentrality<Developer, FileSet> bc = new BetweennessCentrality<Developer, FileSet>(dn
-				.getGraph(), true, true);
-		bc.setRemoveRankScoresOnFinalize(false);
 		log.debug("\tComputing betweenness...");
-		bc.evaluate();
+		bc = new BetweennessCentrality<Developer, FileSet>(dn.getGraph());
 		Collection<Developer> devs = dn.getGraph().getVertices();
 		log.debug("\tLoading dev betweenness & degree scores...");
 		Connection conn = dbUtil.getConnection();
@@ -57,7 +54,7 @@ public class DeveloperNetworkAnalysis {
 		for (Developer dev : devs) {
 			ps.setString(1, dev.getName());
 			ps.setDouble(2, dn.getGraph().degree(dev));
-			ps.setDouble(3, bc.getVertexRankScore(dev));
+			ps.setDouble(3, bc.getVertexScore(dev));
 			ps.addBatch();
 		}
 		log.debug("\tExecuting batch insert ...");
@@ -71,7 +68,7 @@ public class DeveloperNetworkAnalysis {
 			List<String> files = fileSet.getFiles();
 			for (String filepath : files) {
 				ps2.setString(1, filepath);
-				ps2.setDouble(2, bc.getEdgeRankScore(fileSet));
+				ps2.setDouble(2, bc.getEdgeScore(fileSet));
 				ps2.addBatch();
 			}
 		}
@@ -83,11 +80,13 @@ public class DeveloperNetworkAnalysis {
 	private void loadDistances(DeveloperNetwork dn, Transformer<FileSet, Number> fDistance) throws Exception {
 		Graph<Developer, FileSet> graph = dn.getGraph();
 		log.debug("\tComputing distances...");
-		DijkstraShortestPath<Developer, FileSet> sssp = new DijkstraShortestPath<Developer, FileSet>(graph,
-				fDistance, true);
+		DijkstraShortestPath<Developer, FileSet> sssp = new DijkstraShortestPath<Developer, FileSet>(graph, fDistance,
+				true);
+		DijkstraShortestPath<Developer, FileSet> ssspUnweighted = new DijkstraShortestPath<Developer, FileSet>(graph,
+				true);
 		Connection conn = dbUtil.getConnection();
 		PreparedStatement ps = conn
-				.prepareStatement("INSERT INTO DevNetworkDistance(dev1, dev2, distance) VALUES (?,?,?)");
+				.prepareStatement("INSERT INTO DevNetworkDistance(dev1, dev2, distance,unweighteddistance) VALUES (?,?,?,?)");
 		Collection<Developer> devs1 = graph.getVertices();
 		Collection<Developer> devs2 = graph.getVertices();
 		for (Developer dev1 : devs1) {
@@ -96,8 +95,10 @@ public class DeveloperNetworkAnalysis {
 					ps.setString(1, dev1.getName());
 					ps.setString(2, dev2.getName());
 					Number distance = sssp.getDistance(dev1, dev2);
+					Number unweightedDistance = ssspUnweighted.getDistance(dev1, dev2);
 					if (distance != null) {
 						ps.setDouble(3, distance.doubleValue());
+						ps.setDouble(4, unweightedDistance.doubleValue());
 						ps.addBatch();
 					}
 				}
@@ -117,12 +118,17 @@ public class DeveloperNetworkAnalysis {
 		double density = (n - 1) * (n - 2) / 2;
 		density = ((double) e) / density;
 		log.info("\tDensity:      \t" + density);
-		double diameter = DistanceStatistics.diameter(graph, new UnweightedShortestPath<Developer, FileSet>(
-				graph), true);
-		log.info("\tDiameter (non-inf):   \t" + diameter);
+		// double diameter = DistanceStatistics.diameter(graph, new
+		// UnweightedShortestPath<Developer, FileSet>(graph),
+		// true);
+		// log.info("\tDiameter (non-inf):   \t" + diameter);
 	}
-	
+
 	public DeveloperNetwork getDeveloperNetwork() {
 		return dn;
+	}
+
+	public BetweennessCentrality<Developer, FileSet> getBetweennessCentrality() {
+		return bc;
 	}
 }
