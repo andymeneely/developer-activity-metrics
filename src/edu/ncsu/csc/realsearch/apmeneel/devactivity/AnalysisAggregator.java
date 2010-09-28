1198f6fa933dd4cd8679dd29fcb599d3e05be7fd
apmeneel <andy.meneely@gmail.com>
2010-09-28 09:53:11 -0400

    added an analysis aggregator for keeping track of analyses everywhere

---
+++ src/edu/ncsu/csc/realsearch/apmeneel/devactivity/AnalysisAggregator.java
@@ -0,0 +1,21 @@
+package edu.ncsu.csc.realsearch.apmeneel.devactivity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AnalysisAggregator {
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(AnalysisAggregator.class);
+
+	public static void logResult(String experimentDescription, String metricDescription, double result, Connection conn)
+			throws SQLException {
+		log.info(experimentDescription + "\t" + metricDescription + ":\t" + result);
+//		PreparedStatement ps = conn
+//				.prepareStatement("INSERT INTO AnalysisResults(ExperimentDescription, MetricName, MetricValue) VALUES(?,?,?)");
+//		ps.setString(1, experimentDescription);
+//		ps.setString(2, metricDescription);
+//		ps.setDouble(3, result);
+//		ps.executeUpdate();
+//		ps.close();
+	}
+}
