924fe42eb48e5189035b0c07e3d64e7cf72acbf8
Andy Meneely <andy.meneely@gmail.com>
2012-07-03 14:29:34 -0400

    limiting our scope from 5.3.0..5.3.9

--- src/main/sh/git-blame-all.sh
+++ src/main/sh/git-blame-all.sh
@@ -13,7 +13,7 @@ touch $OUTFILE
 for file in `find . -name "*.c" -o -name "*.h" -type f`
 do
 	echo "Working on $file..."
-	for revision in `git log --pretty=oneline $file | awk '{print $1}'`
+	for revision in `git log --pretty=oneline php-5.0.0..php-5.3.9 -- $file | awk '{print $1}'`
 	do
 		echo "***** Revision $revision *****" >> $OUTFILE
 		echo "***** File $file *****" >> $OUTFILE
