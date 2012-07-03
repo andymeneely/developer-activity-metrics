4173db089f7fa4b23858bb9bc11d47d850fbb5c1
Andy Meneely <andy.meneely@gmail.com>
2012-07-03 10:15:13 -0400

    the super-slow version of generating all of the blames

---
+++ src/main/sh/git-blame-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+date
+
+DIR=$1
+OUTFILE=$2
+
+cd $DIR
+
+rm $OUTFILE
+touch $OUTFILE
+
+for file in `find . -name "*.c" -o -name "*.h" -type f`
+do
+	echo "Working on $file..."
+	for revision in `git log --pretty=oneline | awk '{print $1}'`
+	do
+		echo "***** Revision $revision *****" >> $OUTFILE
+		#echo "***** File $file *****" >> $OUTFILE
+		#git blame -l $revision -- $file >> $OUTFILE
+		#echo "***** End *****" >> $OUTFILE
+	done
+done
+
+echo "Done."
+
+date
