b87e6bd524e183d9f652d156039275a419deef84
Andy Meneely <andy.meneely@gmail.com>
2012-07-05 12:45:31 -0400

    took out some annoying debug putses, precious

--- src/main/sh/git-interaction-churn.rb
+++ src/main/sh/git-interaction-churn.rb
@@ -15,14 +15,11 @@ patch_text = `git log -p --unified=0 -1 #{revision} -- #{file}`
 patch_text.each_line { | line |
 	if line.start_with? "Author: " 
 		author = line[8..line.index(' <')].chomp # store just the author name
-		#puts author
 	elsif line.start_with? "@@"
 		#parsing the @@ -a,b +c,d @@
-		puts line
 		lines_deleted_start = line.split(/[ ]+/)[1].split(/[,]+/)[0] #a 
 		lines_deleted_num = line.split(/[ ]+/)[1].split(/[,]+/)[1] # b 
-		lines_added_num = line.split(/[ ]+/)[2].split(/[,]+/)[1] #d 
-		puts "lines_deleted_start: #{lines_deleted_num}"
+		lines_added_num = line.split(/[ ]+/)[2].split(/[,]+/)[1] #d
 
 		#lines_deleted_start isn't ACTUALLy negative...
 		lines_deleted_start = lines_deleted_start.to_i* -1
@@ -47,9 +44,7 @@ patch_text.each_line { | line |
 		# * -l for showing long revision names 
 		if lines_deleted_num > 0 then
 			blame_text = `git blame -l -L #{lines_deleted_start},+#{lines_deleted_num} #{revision}^ -- #{file}`
-			#puts blame_text
 			blame_text.each_line { | blame_line | 
-				puts blame_line
 				if blame_line.include?(author) #does not contain the author in the line?
 					lines_deleted_self+=1
 				else
