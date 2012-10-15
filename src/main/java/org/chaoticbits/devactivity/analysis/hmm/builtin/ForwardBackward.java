8a0738761e817c3a6d703c4972bf24599f9a914c
Andy Meneely <andy.meneely@gmail.com>
2012-10-15 10:47:10 -0400

    commenting and refactoring ForwardBackward

--- src/main/java/org/chaoticbits/devactivity/analysis/hmm/builtin/ForwardBackward.java
+++ src/main/java/org/chaoticbits/devactivity/analysis/hmm/builtin/ForwardBackward.java
@@ -15,42 +15,86 @@ import org.chaoticbits.devactivity.analysis.hmm.IHMMTransition;
 import org.chaoticbits.devactivity.analysis.hmm.IHiddenMarkovModel;
 
 import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.Graph;
 
-public class ForwardBackward<T extends IHMMAlphabet<T>> implements IHMMStateInferencer<T> {
+/**
+ * An implementation of the Forward-Backward algorithm for Hidden Markov Models. Given a trained HMM, we
+ * compute the probability of being in a given state given a particular signal.
+ * 
+ * For starting probabilities, we assume that each initial state is equally likely.
+ * 
+ * @author andy
+ * 
+ * @param <T>
+ */
+public class ForwardBackward<T extends IHMMAlphabet<T>> implements
+		IHMMStateInferencer<T> {
 
 	// p(state | sequence) - the dynamic programming table
 	private Map<StateSeqKey<T>, Double> p_state_seq;
 
-	public Map<IHMMState<T>, Double> logProbInState(IHiddenMarkovModel<T> hmm, List<T> signal) {
+	/**
+	 * Given a model and a signal, compute the probability that the model ends up in a given state. The
+	 * answer is given in the {@link Math.log10} of the probability (to prevent poor double precision).
+	 * 
+	 * @return map - the map of a state to a probability, given this signal
+	 * 
+	 */
+	public Map<IHMMState<T>, Double> logProbInState(IHiddenMarkovModel<T> hmm,
+			List<T> signal) {
 		p_state_seq = new HashMap<StateSeqKey<T>, Double>(signal.size());
-		DirectedGraph<IHMMState<T>, IHMMTransition<T>> g = hmm.getStateGraph();
 
-		startingProbs(hmm, signal, g);
-		dynamicProgrammingStep(hmm, signal, g);
+		startingProbs(hmm, signal);
+		dynamicProgrammingStep(hmm, signal);
 
 		return lastColumn(signal, hmm);
 	}
 
-	private void startingProbs(IHiddenMarkovModel<T> hmm, List<T> signal,
-			DirectedGraph<IHMMState<T>, IHMMTransition<T>> g) {
-		Collection<IHMMTransition<T>> outEdges = g.getOutEdges(hmm.starting());
-		for (IHMMTransition<T> edge : outEdges) {
-			IHMMState<T> state = g.getDest(edge);
+	/**
+	 * Computes the starting probabilities for the first letter in the signal (index=0). Base case for the
+	 * dynamic programming step.
+	 * @param hmm
+	 * @param signal
+	 * @param g
+	 */
+	private void startingProbs(IHiddenMarkovModel<T> hmm, List<T> signal) {
+		DirectedGraph<IHMMState<T>, IHMMTransition<T>> graph = hmm
+				.getStateGraph();
+		Collection<IHMMTransition<T>> startingOutEdges = graph.getOutEdges(hmm
+				.starting());
+		for (IHMMTransition<T> edge : startingOutEdges) {
+			IHMMState<T> state = graph.getDest(edge);
 			Double emitProb = state.emissionProbability(signal.get(0));
 			Double edgeProb = edge.getProb().toDouble();
-			p_state_seq.put(new StateSeqKey<T>(state, 0), log10(edgeProb) + log10(emitProb));
+			p_state_seq.put(new StateSeqKey<T>(state, 0), log10(edgeProb)
+					+ log10(emitProb));
 		}
 	}
 
-	private void dynamicProgrammingStep(IHiddenMarkovModel<T> hmm, List<T> signal,
-			DirectedGraph<IHMMState<T>, IHMMTransition<T>> g) {
+	/**
+	 * Go through the rest of the signal, (index=1..end), and for each state that emits a signal look at the
+	 * incoming edges. For each incoming edge that has a non-zero probability (i.e. it's reachable from this
+	 * signal), compute the probability of reaching this state. The formula is:
+	 * 
+	 * p(state | signal_0..i) = sigma_incomingStatesS( p(S|signal_0..i-1)*inEdge*emissionProb )
+	 * 
+	 * That is, the probability of getting to this state is the probability of getting to any of the previous
+	 * states times the transition probability, times the emission probability.
+	 * 
+	 * @param hmm
+	 * @param signal
+	 */
+	private void dynamicProgrammingStep(IHiddenMarkovModel<T> hmm,
+			List<T> signal) {
+		Graph<IHMMState<T>, IHMMTransition<T>> graph = hmm.getStateGraph();
 		for (int i = 1; i < signal.size(); i++) {
 			for (IHMMState<T> state : hmm.emittingStates()) {
 				double prob = 0.0;
-				for (IHMMTransition<T> inEdge : g.getInEdges(state)) {
-					IHMMState<T> source = g.getSource(inEdge);
+				for (IHMMTransition<T> inEdge : graph.getInEdges(state)) {
+					IHMMState<T> source = graph.getSource(inEdge);
 
-					Double previousProb = p_state_seq.get(new StateSeqKey<T>(source, i - 1));
+					Double previousProb = p_state_seq.get(new StateSeqKey<T>(
+							source, i - 1));
 					if (previousProb == null)
 						continue; // then this state is impossible to get to at this time
 
@@ -66,39 +110,42 @@ public class ForwardBackward<T extends IHMMAlphabet<T>> implements IHMMStateInfe
 	}
 
 	/**
-	 * Go down the last column of our "array" to get the final probabilities for the last symbol in
-	 * the signal for our answer
+	 * Go down the last column of our "array" to get the final probabilities for the last symbol in the
+	 * signal for our answer
 	 * 
 	 * @param signal
 	 * @param hmm
 	 * @return
 	 */
-	private Map<IHMMState<T>, Double> lastColumn(List<T> signal, IHiddenMarkovModel<T> hmm) {
+	private Map<IHMMState<T>, Double> lastColumn(List<T> signal,
+			IHiddenMarkovModel<T> hmm) {
 		int lastColumnIndex = signal.size() - 1;
-		Map<IHMMState<T>, Double> map = new HashMap<IHMMState<T>, Double>(signal.size());
+		Map<IHMMState<T>, Double> map = new HashMap<IHMMState<T>, Double>(
+				signal.size());
 		for (IHMMState<T> state : hmm.emittingStates()) {
-			map.put(state, p_state_seq.get(new StateSeqKey<T>(state, lastColumnIndex)));
+			map.put(state,
+					p_state_seq.get(new StateSeqKey<T>(state, lastColumnIndex)));
 		}
 		return map;
 	}
 
 	/**
-	 * Normally you'd use a 2D array here - but we're using hashtables to do our dyamic programming.
-	 * Normally one dimension is for each non-starting state, and the other dimension is the signal.
+	 * Normally you'd use a 2D array here - but we're using hashtables to do our dyamic programming. Normally
+	 * one dimension is for each non-starting state, and the other dimension is the signal. Hence, we need a
+	 * custom key for a state-sequence_index pairing.
 	 * 
-	 * As we compute our backward probabilities, we'll just look up the i-1th entries in the
-	 * hashtable instead of iterating over the previous column. It's pretty much the same thing
-	 * (hopefully...)
+	 * As we compute our backward probabilities, we'll just look up the i-1th entries in the hashtable
+	 * instead of iterating over the previous column. It's pretty much the same thing (hopefully...)
 	 * 
 	 * @author andy
 	 * 
-	 * @param <T>
+	 * @param <S>
 	 */
-	private class StateSeqKey<T extends IHMMAlphabet<T>> {
-		IHMMState<T> state;
+	private class StateSeqKey<S extends IHMMAlphabet<S>> {
+		IHMMState<S> state;
 		int seqIndex;
 
-		public StateSeqKey(IHMMState<T> state, int seqIndex) {
+		public StateSeqKey(IHMMState<S> state, int seqIndex) {
 			this.state = state;
 			this.seqIndex = seqIndex;
 		}
@@ -134,7 +181,7 @@ public class ForwardBackward<T extends IHMMAlphabet<T>> implements IHMMStateInfe
 			return true;
 		}
 
-		private ForwardBackward getOuterType() {
+		private ForwardBackward<T> getOuterType() {
 			return ForwardBackward.this;
 		}
 
